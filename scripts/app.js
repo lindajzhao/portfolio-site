98892bf7568aaa4dac3b1495d0cdc872e39627b9
Linda Zhao <lindajzhao@gmail.com>
2019-03-04 17:23:33 -0500

    Use 'picture' tag and compressed images

--- scripts/app.js
+++
@@ -1,7 +0,0 @@
-function adjustImage() {
-    if ($(window).width() <= 700) {
-        $('#headshot').attr('src', './assets/headshot-small.png');
-    } else {
-        $('#headshot').attr('src', './assets/headshot.jpg');
-    }
-}
